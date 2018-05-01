c3dc083b65debb7e3a7262df375ddb11fecba6bc
Sean Pierce Sumler <sumler.sean@gmail.com>
2018-05-01 11:48:24 -0700

    Remove unneeded code, format elements

--- index.php
+++ index.php
@@ -1,21 +1,6 @@
 <html>
 <head>
 <script>
-function showHint(str) {
-    if (str.length == 0) { 
-        document.getElementById("txtHint").innerHTML = "";
-        return;
-    } else {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("txtHint").innerHTML = this.responseText;
-            }
-        };
-        xmlhttp.open("GET", "gethint.php?q=" + str, true);
-        xmlhttp.send();
-    }
-}
 function test(e) {
     e.preventDefault()
     var xmlhttp = new XMLHttpRequest();
@@ -33,25 +18,16 @@ function test(e) {
 }  
 </script>
 </head>
-<body>
-
-<div class="section">
-    <p><b>Start typing a name in the input field below:</b></p>
-    <form> 
-        First name: <input type="text" onkeyup="showHint(this.value)">
-    </form>
-    <p>Suggestions: <span id="txtHint"></span></p>
-</div>
-
-<div class="section">
-    <!-- <form action="upload.php" method="post" enctype="multipart/form-data"> -->
-    <form onsubmit="test(event)" enctype="multipart/form-data">
-        Select image to upload:
-        <input type="file" name="fileToUpload" id="fileToUpload">
-        <input type="submit" value="Upload Image" name="submit">
-    </form>
-</div>
+    <body>
+        <div class="section">
+            <!-- <form action="upload.php" method="post" enctype="multipart/form-data"> -->
+            <form onsubmit="test(event)" enctype="multipart/form-data">
+                Select image to upload:
+                <input type="file" name="fileToUpload" id="fileToUpload">
+                <input type="submit" value="Upload Image" name="submit">
+            </form>
+        </div>
 
 
-</body>
+    </body>
 </html>
