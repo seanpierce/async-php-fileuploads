ccf9b816e4d8358a876f0de25c0ce6e1f4a15822
Sean Pierce Sumler <sumler.sean@gmail.com>
2018-05-01 14:27:19 -0700

    Refactor scripts

--- index.php
+++ index.php
@@ -1,27 +1,28 @@
 <html>
 <head>
 <script>
-function test(e) {
+function uploadFile(e) {
     e.preventDefault()
-    var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                //document.getElementById("txtHint").innerHTML = this.responseText;
-            }
-        };
-        var formData = new FormData();
-        formData.append("fileToUpload", document.getElementById("fileToUpload").files[0]);
+    var formData = new FormData()
+    formData.append("fileToUpload", document.getElementById("fileToUpload").files[0])
 
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", "upload.php");
-        xhr.send(formData);  
+    var xhr = new XMLHttpRequest()
+    xhr.onreadystatechange = () => {
+        if (this.readyState == 4 && this.status == 200)
+            console.log('upload complete')
+    }
+    xhr.onprogress = (progress) => {
+        if (progress.lengthComputable)
+            console.log(progress.loaded / progress.total)
+    }
+    xhr.open("POST", "upload.php")
+    xhr.send(formData); 
 }  
 </script>
 </head>
     <body>
         <div class="section">
-            <!-- <form action="upload.php" method="post" enctype="multipart/form-data"> -->
-            <form onsubmit="test(event)" enctype="multipart/form-data">
+            <form onsubmit="uploadFile(event)" enctype="multipart/form-data">
                 Select image to upload:
                 <input type="file" name="fileToUpload" id="fileToUpload">
                 <input type="submit" value="Upload Image" name="submit">
