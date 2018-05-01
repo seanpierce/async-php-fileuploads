53e1eda6126e14b8052de47590defc1fd0393e43
Sean Pierce Sumler <sumler.sean@gmail.com>
2018-05-01 16:29:11 -0700

    Simplified scripts, included response messages for enhanced err handling

--- upload.php
+++ upload.php
@@ -1,49 +1,54 @@
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$dir = "uploads/";
+$file = $_FILES["fileToUpload"];
+$path = $dir . basename($file["name"]);
+$error = false;
+$type = strtolower(pathinfo($path,PATHINFO_EXTENSION));
+$response;
 
 // Check if image file is a actual image or fake image
 if (isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
+        $error = false;
     } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
+        $response->errors[] = "File is not an image.";
+        $error = true;
     }
 }
 
 // Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
+if (file_exists($path)) {
+    $response->errors[] = "Sorry, file already exists..";
+    $error = true;
 }
 
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
+    $response->errors[] = "Sorry, your file is too large.";
+    $error = true;
 }
 
 // Allow certain file formats
-if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
+if ($type != "jpg" && $type != "png" && $type != "jpeg" && $type != "gif" ) {
+    $response->errors[] = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $error = true;
 }
 
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    return http_response_code(400);
+// Check if $error is true
+if ($error) {
+    http_response_code(400);
+    echo json_encode($response);
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        return http_response_code(200);
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $path)) {
+        $response->message = "Success";
+        echo json_encode($response);
     } else {
-        return http_response_code(400);
+        $response->error[] = "Something went wrong. File was not uploaded.";
+        echo json_encode($response);
+        http_response_code(400);
     }
 }
 ?>
