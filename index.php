3a537abf43a5c060d36a70d511e08686daf5a5e1
Sean Pierce Sumler <sumler.sean@gmail.com>
2018-05-01 16:27:47 -0700

    Moved scripts to separate file

--- index.php
+++ index.php
@@ -1,24 +1,7 @@
 <html>
-<head>
-<script>
-function uploadFile(e) {
-    e.preventDefault()
-    var formData = new FormData()
-    formData.append("fileToUpload", document.getElementById("fileToUpload").files[0])
-
-    var xhr = new XMLHttpRequest()
-    xhr.onreadystatechange = (state) => {
-        if (state.target.readyState === 4 && state.target.status === 200)
-            console.log('Upload complete!')
-    }
-    xhr.upload.onprogress = (progress) => {
-        console.log((progress.loaded / progress.total) * 100)
-    }
-    xhr.open("POST", "upload.php")
-    xhr.send(formData); 
-}  
-</script>
-</head>
+    <head>
+        <script src="scripts.js"></script>
+    </head>
     <body>
         <div class="section">
             <form onsubmit="uploadFile(event)" enctype="multipart/form-data">
